51498abd3316bf0805e64baaf6706f4b2a0157af
Sudar Meruva <meruva_s@yaho.com>
2019-12-20 19:14:08 -0500

    added 5 JS questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,12 +1,27 @@
 var questions = [
   {
-    question: "What is a computer?",
-    choices: ["A noun.", "A verb."],
-    answer: "A noun."
+    question: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
   },
   {
-    question: "JavaScript is awesome.",
-    choices: ["True", "False"],
-    answer: "True"
+    question: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    question: "Arrays in Javascript can be used to store",
+    choices: ["Numbers and Strings", "Other Arrays", "Boolean", "all of the above"],
+    answer: "all of the above"
+  },
+  {
+    question: "String values must be encolsed within _____",
+    choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+    answer: "quotes"
+  },
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    answer: "console.log"
   }
 ];
